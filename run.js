f221ec23b50ff3020568a0ad403e80450f008aa1
Nicolas <nicolas.meseth@googlemail.com>
2017-11-23 13:58:08 +0100

    Added further Wrappers and support for sensors

--- run.js
+++ run.js
@@ -1,45 +1,48 @@
 var { DeviceManager } = require('./lib/DeviceManager.js');
+var moment = require('moment');
+var tinkerforge = require('tinkerforge');
+
+// Get the loggers
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+var sensorsLogger = log4js.getLogger('sensors');
 
 var dm = new DeviceManager();
 
 /* Initialize Device Manager and start
  * when finished */
-dm.initialize().then(start);
+dm.initialize().then(start).catch(handleError);
 
 function start() {
 
-    var co2 = dm.get("CVW");
-    co2.registerListener(co2Changed);
+    var co2Sensor = dm.get("CVW");
+    co2Sensor.setCallbackInterval(500);
+    co2Sensor.registerLogger(sensorsLogger);
+    co2Sensor.registerListener(co2Changed);
 
-    /*
-    var temperature = dm.get("zfG");
-    temperature.registerListener(temperatureChanged);
-    */
+    var temperatureSensor = dm.get("zta");
+    temperatureSensor.setCallbackInterval(500);
+    temperatureSensor.registerLogger(sensorsLogger);
 
-    /*
-    var distanceIRBricklet = dm.get("xue");
-    distanceIRBricklet.registerListener(distanceChangedIR);
-    distanceIRBricklet.setCallbackInterval(200);
+    var ambientLightSensor = dm.get("yBJ");
+    ambientLightSensor.setCallbackInterval(500);
+    ambientLightSensor.registerLogger(sensorsLogger);
 
-    var distanceUSBricklet = dm.get("zod");
-    distanceUSBricklet.registerListener(distanceChangedUS);
-    distanceUSBricklet.setCallbackInterval(200);
+    var barometerSensor = dm.get("vNL");
+    barometerSensor.setCallbackInterval(1000);
+    barometerSensor.registerLogger(sensorsLogger);
 
-    */
+    var uvLightSensor = dm.get("xnC");
+    uvLightSensor.setCallbackInterval(1000);
+    uvLightSensor.registerLogger(sensorsLogger);
+
 }
 
 function co2Changed(co2) {
     console.log(co2);
 }
 
-function temperatureChanged(t) {
-    console.log(t / 100.0);
-}
+function handleError(err) {
+    logger.error(err);
 
-function distanceChangedIR(distance) {
-    console.log("IR: " + distance / 10.0);
-}
-
-function distanceChangedUS(distance) {
-    console.log("US : " + distance / 10.0);
 }
