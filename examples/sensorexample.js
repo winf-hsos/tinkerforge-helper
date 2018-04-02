eb1fc3b2a883cb50dc365b683065f39a92411a0e
Nicolas <nicolas.meseth@googlemail.com>
2018-04-02 22:00:15 +0200

    Small changes

--- examples/sensorexample.js
+++ examples/sensorexample.js
@@ -1,29 +1,11 @@
 var { DeviceManager } = require('../lib/DeviceManager.js');
 
 var dm = new DeviceManager();
-dm.initialize().then(start).catch(handleError);
+dm.initialize().then(startExperiment).catch(handleError);
 
-function start() {
 
-    var humiditySensor = dm.get("Dge");
-    humiditySensor.setCallbackInterval(250);
-    //humiditySensor.registerListener(newHumidtyOrTemperatureValue);
-
-    var accelerometerSensor = dm.getByDeviceIdentifier(250);
-    //accelerometerSensor.registerListener(acclerationChanged);
-
-    //dm.get("Dov").setColor(0, 255, 0);
-
-    var rgbButton = dm.getByDeviceIdentifier(282);
-    rgbButton.setColor(0, 255, 255);
-}
-
-function newHumidtyOrTemperatureValue(valueObj) {
-    //    console.log(valueObj);   
-}
-
-function acclerationChanged(valueObj) {
-    console.dir(valueObj);
+function startExperiment() {
+    console.log("Start sensor experiment...");
 }
 
 function handleError(err) {
